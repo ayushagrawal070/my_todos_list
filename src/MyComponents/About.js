c2f47d785c953fec88642246ea156fac25bfb213
Ayush Agrawal <ayushagrawal070@gmail.com>
2021-06-25 13:11:07 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            this is an about component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum qui voluptatum odit ipsam reprehenderit, eligendi non dolor deserunt sit distinctio! Deserunt libero possimus non repellendus! Dolores, qui ipsum enim eum ad iusto ducimus cupiditate.</p>
+        </div>
+    )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <footer className="footer bg-dark py-3 mt-auto">
+            <div className="container text-center">
+                <span className="text-muted">&copy; My Todos List 2021</span>
+            </div>
+        </footer>
+    )
+}
